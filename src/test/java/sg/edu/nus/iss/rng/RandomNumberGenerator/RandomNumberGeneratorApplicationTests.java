7b333d1e769e3a2155762a5e9675f214c3ab6467
Saravanan Munusamy <saravananmunusamy@Saravanans-MacBook-Pro.local>
2022-03-01 15:25:00 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/rng/RandomNumberGenerator/RandomNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.rng.RandomNumberGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
